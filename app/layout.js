d24fa4357a0de0a24cffe4712181b3674c5512db
Harsh Gupta <harshgupta0907@gmail.com>
2025-11-27 23:57:40 +0530

    more progress

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
